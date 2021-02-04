34c6fa438184db6437d81b526e6deb7eacb51b87
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2021-02-04 14:13:17 +0800

    新增首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,64 +1,26 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
-  },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    // if(!app.globalData.userInfo){
-    //   wx.showModal({
-    //     title:"未登录",
-    //     content:"您未登录，请登录后在访问！",
-    //     showCancel:false,
-    //     confirmColor:"#46b989",
-    //     confirmText:"去登陆",
-    //     success:(res)=>{
-    //       console.log(res)
-    //       if(res.confirm){
-    //         wx.reLaunch({
-    //           url: '/pages/login/login',
-    //         })
-    //       }
-    //     }
-    //   })
-    //   return
-    // }
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+    background: ['/static/img/home/banner2.jpg', '/static/img/home/banner1.jpg', '/static/img/home/banner3.jpg'],
+    indicatorDots: true,
+    vertical: true,
+    circular:true,
+    autoplay: false,
+    interval: 2000,
+    duration: 500,
+    homeMenuModule:[
+      {
+        name:"报修",
+        icon:"icon-baoxiu qm-menu-color1",
+        link:"/pages/login/login",
+      },
+      {
+        name:"日志",
+        icon:"icon-danganguanli",
+        link:"/pages/logs/logs"
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    ]
+  },
+  linkNavigator(url){
+    console.log(url)
   }
 })
