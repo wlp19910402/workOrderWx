94f141b74d6346bc0e1d76d4cb6f81c6af7e76bd
王莉萍 <5556929+wlp6897@user.noreply.gitee.com>
2021-03-04 15:31:57 +0800

    修改日期

--- components/portfolio/qm-portfolio-update-consumable-info/index.js
+++ components/portfolio/qm-portfolio-update-consumable-info/index.js
@@ -7,5 +7,29 @@ Component({
       type: Object,
       value: {}
     }
+  },
+  data:{
+    expirationTime:""
+  },
+  methods:{
+    formatDate(data) {
+      if (isNaN(data) && !isNaN(Date.parse(data))) {
+        let date = new Date(data)
+        let year = date.getFullYear()
+        let month = date.getMonth() + 1
+        let day = date.getDate()
+        return year + "-" + (month < 10 ? '0' : '') + month + "-" + (day < 10 ? '0' : '') + day
+      } else {
+        return ""
+      }
+    },
+  },
+  observers: {
+    'workConsumables': function(workConsumables) {
+      // 在 numberA 或者 numberB 被设置时，执行这个函数
+      this.setData({
+        expirationTime: this.formatDate(workConsumables.expirationTime)
+      })
+    }
   }
 })
